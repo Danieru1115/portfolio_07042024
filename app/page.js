a0746df5c0afefe915b0922a471a8bf57db78a6c
HyperDanirio <danieru1115@proton.me>
2024-08-02 17:54:57 +0800

    Daniel 08022024

---
+++ app/page.js
@@ -0,0 +1,16 @@
+'use client'
+import Header from "@/components/Header";
+import Pay from "@/components/Pay";
+import Head from "@/components/Head";
+import Image from "next/image";
+
+export default function Home() {
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white gap-20">
+      <Header/>
+      <Head/>
+      <Pay/>
+    </main>
+  );
+}
