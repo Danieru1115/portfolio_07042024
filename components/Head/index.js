acd029d44f3fed413376ee7ac004231863976cb4
HyperDanirio <danieru1115@proton.me>
2024-08-09 22:34:51 +0800

    Dani

--- components/Head/index.js
+++ components/Head/index.js
@@ -7,7 +7,7 @@ const Head = () => {
     <div className="w-full h-[48rem] flex justify-start items-center py-6 lg:mt-10 xl:mt-20">
       <div className='w-1/2 h-full flex flex-col justify-center items-start z-20 ml-20 gap-8'>
       <h1 className='text-purple1 text-4xl font-semibold'>Daniel Bekhbat Jose Alberto</h1>
-      <h2 className='text-black3 w-[642px] text-lg font-normal '>Hi! I'm a passionate UI/UX Designer, Graphic Designer, and Web/Mobile Developer always looking to expand my knowledge. I'm driven to excel not only in the creative and technical aspects of digital design, but also in understanding the business side of things.</h2>
+      <h2 className='text-black3 w-[642px] text-lg font-normal '>Hi! Im a passionate UI/UX Designer, Graphic Designer, and Web/Mobile Developer always looking to expand my knowledge. Im driven to excel not only in the creative and technical aspects of digital design, but also in understanding the business side of things.</h2>
       </div>
       <div className='w-1/2 h-full flex justify-end items-center z-0'>
       <Image
