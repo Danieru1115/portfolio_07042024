2a413fa545dd4aa66682af1086d72216a9e36d60
HyperDanirio <danieru1115@proton.me>
2024-08-15 19:17:38 +0800

    Daniel 08152024/3.6

--- app/moreExperiences/page.js
+++ app/moreExperiences/page.js
@@ -32,7 +32,7 @@ export default function Experiences (){
     return(
         <div className="w-full h-full flex flex-col justify-start items-center px-6 gap-12">
             <div className="flex justify-between items-center w-full h-20 bg-white/50 backdrop-blur-md top-0 left-0 fixed z-50 px-8">
-                <div className="w-1/3 h-full flex justify-start items-center "><button onClick={() => router.back()}  className=" bg-white rounded-full p-1"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div className="w-1/3 h-full flex justify-start items-center "><button onClick={() => router.back()}  className="  rounded-full p-1"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M20 4V5.4C20 8.76031 20 10.4405 19.346 11.7239C18.7708 12.8529 17.8529 13.7708 16.7239 14.346C15.4405 15 13.7603 15 10.4 15H4M4 15L9 10M4 15L9 20" stroke="purple" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
 </button></div>
@@ -40,7 +40,7 @@ export default function Experiences (){
                 <div className="w-1/3 h-full"></div>
             </div>
             <div className="flex-justify-center items-start py-4 mt-20">
-                <div className="w-full h-auto grid grid-cols-5 gap-4 gap-y-12">
+                <div className="w-full h-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 gap-y-12">
                 {experiences.map((experience, index) => (
           <a href={experience.link} target="_blank" rel="noopener noreferrer" key={index} className="mb-4 py-6 px-12 rounded-2xl border border-divider1 border-solid cursor-default w-80 h-auto flex flex-col justify-center items-start ">
             <div>{getStarRating(parseInt(experience.ranking))}</div>
