211872127992f9e47169e80b6adcf6c7ec89e186
HyperDanirio <danieru1115@proton.me>
2024-08-17 17:42:16 +0800

    Daniel 17082024/6

--- app/data/ExperienceInfo.js
+++ app/data/ExperienceInfo.js
@@ -1,201 +1,40 @@
 export const experienceData = [
+  {
+    "title": "Intermed Hospital IT Intern",
+    "role": "IT",
+    "ranking": "10",
+    "company": "Intermed Hospital"
+  },
     {
-      "title": "ETT Inner Software",
+      "title": "Erdenes Tavantolgoi IT Intern",
       "role": "Software Development",
-      "ranking": "8",
+      "ranking": "9",
       "company": "ErdenesTavanTolgoi"
     },
     {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "8",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "10",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "4",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
-      "ranking": "3",
-      "company": "ErdenesTavanTolgoi"
-    },
-    {
-      "title": "ETT Inner Software",
-      "role": "Software Development",
+      "title": "Prodent LLC Graphic Designer",
+      "role": "Graphic Designer",
       "ranking": "5",
-      "company": "ErdenesTavanTolgoi"
+      "company": "Prodent LLC"
+    },
+    {
+      "title": "Ismon Agrotech Software Developer",
+      "role": "Software Development",
+      "ranking": "8",
+      "company": "IsmonAgrotech LLC"
+    },
+    {
+      "title": "Paimatrix LLC Software Developer",
+      "role": "Software Development",
+      "ranking": "6",
+      "company": "Paimatrix LLC"
+    },
+    {
+      "title": "Ulger.mn Frontend Developer",
+      "role": "Frontend Development",
+      "ranking": "10",
+      "company": "Ulger LLC"
     }
+
   ];
   
--- app/data/PreviousArtWorkInfo.js
+++ app/data/PreviousArtWorkInfo.js
@@ -6,39 +6,33 @@ export const ArtWorkData = [
         type : 'Illustration'
     },
     {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Cstle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
+        image : 'https://cdna.artstation.com/p/assets/images/images/078/472/332/large/daniel-bekhbat-old-house.jpg?1722217083',
+        title : 'Japanese style Old House',
+        link : 'https://www.artstation.com/artwork/AZw5m5',
         type : 'Illustration'
     },
     {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Castle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
+        image : 'https://cdnb.artstation.com/p/assets/images/images/078/520/431/large/daniel-bekhbat-sea-sight.jpg?1722342906',
+        title : 'Sea and the Girl',
+        link : 'https://www.artstation.com/artwork/dyJove',
         type : 'Illustration'
     },
     {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Cstle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
+        image : 'https://cdnb.artstation.com/p/assets/images/images/077/612/781/large/daniel-bekhbat-woman1.jpg?1719907896',
+        title : 'Sad Woman',
+        link : 'https://www.artstation.com/artwork/kQVnZ6',
         type : 'Illustration'
     },
     {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Castle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
+        image : 'https://cdna.artstation.com/p/assets/images/images/077/848/974/large/daniel-bekhbat-sx-girl.jpg?1720526642',
+        title : 'Sad girl, Ima wa iindayo',
+        link : 'https://www.artstation.com/artwork/dKzAEJ',
         type : 'Illustration'
     },
     {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Cstle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
-        type : 'Illustration'
-    },
-    {
-        image : 'https://cdnb.artstation.com/p/assets/images/images/077/622/141/large/daniel-bekhbat-castle-completed.jpg?1719925761',
-        title : 'Sakura tree and Cstle',
-        link : 'https://www.artstation.com/artwork/vDnxZA',
+        image : 'https://cdnb.artstation.com/p/assets/images/images/078/287/913/large/daniel-bekhbat-haunted-appartment.jpg?1721714777',
+        title : 'Haunted appartmen',
+        link : 'https://www.artstation.com/artwork/Al33By',
         type : 'Illustration'
     }
 ]
